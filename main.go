72628872a5a677785e13db65536506b26c9d4de1
Changhoi Kim <0417kch@naver.com>
2023-09-12 10:17:52 +0900

    make cli entry on main function

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+)
 
+func main() {
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+	app := &cli.App{
+		Name:    "tig",
+		Usage:   "stupid content tracker",
+		Version: "v0.0.0",
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		logger.Fatal(err)
+	}
 }
