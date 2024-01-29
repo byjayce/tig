d5279f5d1cd607bae66d4f29a36df62f5e4bfff3
Changhoi Kim <changhoi0522@gmail.com>
2024-01-29 21:11:07 +0900

    00. Init (#1)

---
+++ internal/workingcopy/init_test.go
@@ -0,0 +1,58 @@
+package workingcopy
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"gopkg.in/yaml.v3"
+	"os"
+	"path/filepath"
+)
+
+var _ = Describe("Init", func() {
+	When("Init() 함수를 호출하면", func() {
+		var tempDir string
+		BeforeEach(func() {
+			tempDir = GinkgoT().TempDir()
+		})
+
+		It("config 파일과 head 파일이 생성된다.", func() {
+			config := InitConfig{
+				Core: InitCoreConfig{
+					Bare: false,
+				},
+			}
+
+			initParam := InitParam{
+				WorkingCopyPath: tempDir,
+				Config:          config,
+			}
+
+			configBuf, err := yaml.Marshal(config)
+			if err != nil {
+				Fail(err.Error())
+			}
+
+			Expect(Init(initParam)).Should(BeNil())
+
+			f, err := os.Stat(filepath.Join(tempDir, configFileName))
+			Expect(err).Should(BeNil())
+			Expect(f.IsDir()).Should(BeFalse())
+
+			buf, err := os.ReadFile(filepath.Join(tempDir, configFileName))
+			Expect(err).Should(BeNil())
+			Expect(buf).Should(Equal(configBuf))
+
+			f, err = os.Stat(filepath.Join(tempDir, headFileName))
+			Expect(err).Should(BeNil())
+			Expect(f.IsDir()).Should(BeFalse())
+
+			d, err := os.Stat(filepath.Join(tempDir, objectsDirName))
+			Expect(err).Should(BeNil())
+			Expect(d.IsDir()).Should(BeTrue())
+
+			d, err = os.Stat(filepath.Join(tempDir, refsDirName))
+			Expect(err).Should(BeNil())
+			Expect(d.IsDir()).Should(BeTrue())
+		})
+	})
+})
